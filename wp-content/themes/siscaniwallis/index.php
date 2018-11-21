5d0d83c38df7cbace99996c55fee2171ce052b30
RafaelEsaa <esaarafael@hotmail.com>
2018-11-21 10:38:33 -0400

    first commit

---
+++ wp-content/themes/siscaniwallis/index.php
@@ -0,0 +1,17 @@
+
+<html>
+    <header>
+    <?php wp_head();?>
+    </header>
+    <body>
+    <div class="row full-width">
+        <div class="col-md-12">
+            <p>Holaaa</p>
+        </div>
+    </div>        
+        <p class="prueba">HOLA RAFAEL!</p>
+        <footer>
+            <?php wp_footer();?>
+        </footer>
+    </body>
+</html>
